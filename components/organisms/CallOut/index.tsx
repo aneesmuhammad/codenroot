0be6305902a8c256533e519207bf18ce56688130
aneesmuhammad <76421535+aneesmuhammad@users.noreply.github.com>
2024-09-23 13:18:45 +0000

    fix

--- components/organisms/CallOut/index.tsx
+++ components/organisms/CallOut/index.tsx
@@ -13,7 +13,7 @@ const CallOut = () => {
         </div>
         <div className="flex flex-col gap-5 w-full sm:w-fit sm:flex-row lg:w-full lg:justify-end">
           <ButtonLink value="contact us" href="/contact" />
-          <ButtonLink value="FQA" color="white" style="light" href="/faq" />
+          <ButtonLink value="FAQ" color="white" style="light" href="/faq" />
         </div>
       </section>
       <LineDivider />
